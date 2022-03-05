cd27896f339d72c8b732c952754248d0e4b72019
Nitish Kumar Singh <99019836+nitishsinghkushwaha@users.noreply.github.com>
2022-03-05 10:59:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "Nitish", "Dheeraj", "Deepak", "Silpee", "Dulari", "Anjali", "Ritesh", "Manu", "Abhishek"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
